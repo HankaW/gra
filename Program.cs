603235b614c7648eeb878070a2542eec9ee29fa6
HankaW <123004854+HankaW@users.noreply.github.com>
2023-08-03 14:43:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Gra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // {przestrzen nazw}.{klasa}.{metoda}
+           Gra game = new Gra();
+           //Gra.Gra.start()
+           game.start();
+          
+        }
+    }
+}
